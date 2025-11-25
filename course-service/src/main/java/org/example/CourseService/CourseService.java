55d9d850d4d16cc244353c8d837ef8e3627ebbab
ArtemSoldatovV <solvladislav777@gmail.com>
2025-11-26 00:12:28 +0300

    version 0.5.1
    removing excess

--- course-service/src/main/java/org/example/CourseService/CourseService.java
+++ course-service/src/main/java/org/example/CourseService/CourseService.java
@@ -19,31 +19,5 @@ public class CourseService {
                 .map(course -> ResponseEntity.ok(course))
                 .orElse(ResponseEntity.notFound().build());
     }
-
-    @PostMapping
-    public ResponseEntity<Course> createCourse(@RequestBody Course course) {
-        Course savedCourse = courseRepository.save(course);
-        return ResponseEntity.status(HttpStatus.CREATED).body(savedCourse);
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<Course> updateCourse(@PathVariable Long id, @RequestBody Course courseDetails) {
-        return courseRepository.findById(id)
-                .map(existingCourse -> {
-                    existingCourse.setName(courseDetails.getName());
-                    Course updatedProduct = courseRepository.save(existingCourse);
-                    return ResponseEntity.ok(updatedProduct);
-                })
-                .orElse(ResponseEntity.notFound().build());
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Object> deleteCourse(@PathVariable Long id) {//изначально был ResponseEntity<Void>
-        return courseRepository.findById(id)
-                .map(course -> {
-                    courseRepository.delete(course);
-                    return ResponseEntity.noContent().build();
-                })
-                .orElse(ResponseEntity.notFound().build());
-    }
 }
+
